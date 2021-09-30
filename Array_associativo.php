0cd575ab3cb7a82f9ce66bdb0a81cacc353e6f7c
Nicola Cristian <nicola.cristian@osdbsesto>
2021-09-30 12:48:43 +0200

    init repo

---
+++ Array_associativo.php
@@ -0,0 +1,24 @@
+<html>
+
+<body>
+    <?php
+
+    $nome = array("a" => "piatto", "b" => "10");
+
+    foreach ($nome as $v) {  //Stampo valore contenuto elementi array
+
+        echo $v;
+    }
+
+    foreach ($nome as $v) {  //Stampo valore contenuto elementi array
+
+        echo "key".$k."valore".$v."<br>";        //Stampo key + valore    il punto serve per 
+    }
+
+    ?>
+    
+</body>
+
+
+
+</html>
